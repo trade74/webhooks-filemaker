e117fec06f7068cf52447ed562ccb6a745a620b1
Stuart Russell <stuart@x2network.net>
2024-01-10 18:59:39 +1100

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for webhooks-filemaker
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace craftyfm\craftwebhooksfilemaker\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * webhooks-filemaker settings
+ */
+class Settings extends Model
+{
+}
